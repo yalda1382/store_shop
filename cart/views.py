6c29f115fed8d968406cc21c864a1cf3ced6130f
yalda <168217914+yalda1382@users.noreply.github.com>
2026-01-07 22:59:12 +0330

    Update views.py

--- cart/views.py
+++ cart/views.py
@@ -5,12 +5,7 @@ from .cart import Cart
 from shop.models import Product
 
 
-# def cart_summary(request):
-#     cart=Cart(request)
-#     cart_products=cart.prods()
-#     quintities=cart.get_quants()
-#     total=cart.get_totals()
-#     return render(request, 'cart_summary.html', {'cart_products': cart_products, 'quintities': quintities, 'total': total})
+
 
 def cart_summary(request):
     cart = Cart(request)
@@ -66,65 +61,3 @@ def cart_delete(request):
         return JsonResponse({'product': product_id})
 
 
-# def cart_update(request):
-#    cart = Cart(request)
-
-#    if request.method == "POST" and request.POST.get('action') == 'post':
-#         product_id = int(request.POST.get('product_id'))
-#         product_qty = int(request.POST.get('product_qty'))
-        
-#         cart.update(product= product_id, quantity= product_qty)
-#         messages.success(request, 'Cart updated successfully')
-#         return JsonResponse({'qty': product_qty})
-# def cart_delete(request):
-#     cart = Cart(request)
-
-#     if request.POST.get('action')  == 'post':
-#         product_id = int(request.POST.get('product_id'))
-#         messages.success(request, 'Item removed from cart successfully')
-#         cart.delete(product=product_id)
-#         return JsonResponse({'product': product_id})
-
-
-# from django.shortcuts import render, get_object_or_404
-# from django.http import JsonResponse
-# from .cart import Cart
-# from shop.models import Product
-
-
-# def cart_summary(request):
-#     cart = Cart(request)
-#     cart_products = cart.prods()
-#     quantities = cart.get_quants()
-
-#     return render(request, 'cart_summary.html', {
-#         'cart_products': cart_products,
-#         'quantities': quantities
-#     })
-
-
-# def cart_add(request):
-#     cart = Cart(request)
-
-#     if request.method == "POST" and request.POST.get('action') == 'post':
-#         product_id = int(request.POST.get('product_id'))
-#         product_qty = int(request.POST.get('product_qty'))
-#         product = get_object_or_404(Product, id=product_id)
-
-#         cart.add(product=product, quantity=product_qty)
-
-#         return JsonResponse({'qty': len(cart)})
-
-
-# def cart_update(request):
-#     cart = Cart(request)
-
-#     if request.method == "POST" and request.POST.get('action') == 'post':
-#         product_id = int(request.POST.get('product_id'))
-#         product_qty = int(request.POST.get('product_qty'))
-
-#         cart.update(product=product_id, quantity=product_qty)
-
-#         return JsonResponse({'qty': product_qty})
-# def cart_remove(request):
-#     pass
